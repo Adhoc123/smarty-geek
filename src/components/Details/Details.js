77266e12f4af2c30eb1f23b74681e357ba3c7c46
Adhoc123 <emonhasan1010@gmail.com>
2022-10-11 21:27:56 +0600

    fixed quiz section

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -5,11 +5,12 @@ import './Details.css';
 
 const Details = () => {
     const quizDetails = useLoaderData();
-    const {id, name, logo, questions} = quizDetails.data;
+    const {id, name, logo, questions, total} = quizDetails.data;
+    console.log(quizDetails.data)
     return (
         <div className='details'>
             <img src={logo}></img>
-            <h2>{name}</h2>
+            <h2 >Total {total} quiz is given for {name}</h2>
             {
                 questions.map(question=><Question
                     question={question.question}
