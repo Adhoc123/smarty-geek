8445d3757fe40b5cd559bc11463174a3d9a3137f
Adhoc123 <emonhasan1010@gmail.com>
2022-10-11 12:45:12 +0600

    fixed about section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import geek from '../../images/geek.png';
+import './Home.css';
 
 const Home = () => {
     return (
         <div>
-            <h1>From Home</h1>
+            <div className='about'>
+                <img src={geek}></img>
+                <h5>Smart geek is a place where you can learn through online. We have experts from
+                     different domain. Here we are trying to bridge the gap between industry and consumers. 
+                     By providing world class module we are step ahead in this sector all around the world. 
+                     You are welcomed to check our different sections and learn from experts.
+                </h5>
+            </div>
         </div>
     );
 };
