4ae264082b7dbd6b771923e9e98a0f8c20762ec4
Adhoc123 <emonhasan1010@gmail.com>
2022-10-11 22:30:13 +0600

    added toast

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -6,15 +6,16 @@ import './Details.css';
 const Details = () => {
     const quizDetails = useLoaderData();
     const {id, name, logo, questions, total} = quizDetails.data;
-    console.log(quizDetails.data)
+    console.log(questions)
     return (
         <div className='details'>
             <img src={logo}></img>
-            <h2 >Total {total} quiz is given for {name}</h2>
+            <h2 >This quiz is prepared for {name}</h2>
             {
                 questions.map(question=><Question
                     question={question.question}
                     options={question.options}
+                    correctAnswer={question.correctAnswer}
                 ></Question>)
             }
            
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './Question.css';
 
-const Question = ({question,options}) => {
+const Question = ({question,options,correctAnswer}) => {
     
     let cleanQuestion = question.replace( /(<([^>&]+)>)/ig, '');
     cleanQuestion = cleanQuestion.replace(/&nbsp;/g,'')
-    // let cleanOption = option.replace( /(<([^>&]+)>)/ig, '');
+    const handle = (option) =>{
+       if(option===correctAnswer){
+         alert('right')
+       }
+       else{
+        alert('wrong')
+       }
+    }
     return (
         <div className='question'>
             <fieldset>  
-                    <legend>{cleanQuestion}</legend>
+            <legend>{cleanQuestion}</legend>
             {
                 options.map(option=>
                     <div>
-                      <input type="radio" id="huey" name="drone" value="huey"
+                      <input onClick={()=>handle(option)} type="radio" id="huey" name="drone" value="huey"
                              checked/>
                       <label for="huey">{option}</label>
                     </div> 
