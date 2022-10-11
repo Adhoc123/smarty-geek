792499f65542ce2e33ce14af08c954396a8d0cde
Adhoc123 <emonhasan1010@gmail.com>
2022-10-12 01:05:03 +0600

    toast added

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './Question.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Toaster } from "react-hot-toast";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEye} from '@fortawesome/free-solid-svg-icons';
 
 const Question = ({question,options,correctAnswer}) => {
     
@@ -7,22 +12,30 @@ const Question = ({question,options,correctAnswer}) => {
     cleanQuestion = cleanQuestion.replace(/&nbsp;/g,'')
     const handle = (option) =>{
        if(option===correctAnswer){
-         alert('right')
+        toast.info('hello');
        }
-       else{
-        alert('wrong')
-       }
+
     }
+    const showToastMessage = () => {
+        toast.info('Correct Answer: '+correctAnswer, {
+            position: toast.POSITION.TOP_CENTER
+        });
+    };
     return (
         <div className='question'>
-            <fieldset>  
-            <legend>{cleanQuestion}</legend>
+            <fieldset> 
+            <div className='quiz-header'>
+                <legend>{cleanQuestion}</legend>
+                    <FontAwesomeIcon onClick={showToastMessage} id='icon' icon={faEye} />
+                    <ToastContainer/>
+            </div>
             {
                 options.map(option=>
                     <div>
-                      <input onClick={()=>handle(option)} type="radio" id="huey" name="drone" value="huey"
-                             checked/>
-                      <label for="huey">{option}</label>
+                      <input onClick={handle(option)}   type="radio" id="huey" name="drone" value="huey"
+                             checked/>   
+                      {/* {handle(option)===correctAnswer?<ToastContainer/>:'heloo'} */}
+                      <label for="huey">{option}</label>                      
                     </div> 
                 )
             }
